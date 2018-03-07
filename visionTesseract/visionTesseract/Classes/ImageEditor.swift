cded54560a4e97b80a57c78ab1fa1c99ee05c8d2
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-03-07 09:19:31 -0600

    Se agregan archivos para fitros de imagenes
    Debido a que no es muy eficiente el filtro que ofrece Tesseract, se tomo la iniciativa de crear uno propio.
    
    GAR

---
+++ visionTesseract/visionTesseract/Classes/ImageEditor.swift
@@ -0,0 +1,20 @@
+//
+//  ImageEditor.swift
+//  visionTesseract
+//
+//  Created by Luis Genaro Arvizu Vega on 06/03/18.
+//  Copyright © 2018 HP501865. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+extension UIImage{
+    
+    
+    func configuration(){
+        let context = CIContext()
+        //let filter = CIFilter.filterNames(inCategory: x)
+    }
+    
+}
