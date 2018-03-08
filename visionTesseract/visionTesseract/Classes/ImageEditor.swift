0a0143704455eacff6e00c973226a96510126abc
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-03-08 09:26:37 -0600

    Se agregan funciones para colocar filtros a las imagenes
    Para dar un trato a la imagen, mas optimo se le agrego un filtro personalizado, este pone casí toda la imagen en blanco y el textro en negro, falta optimizar el tema del brillo.

--- visionTesseract/visionTesseract/Classes/ImageEditor.swift
+++ visionTesseract/visionTesseract/Classes/ImageEditor.swift
@@ -3,7 +3,7 @@
 //  visionTesseract
 //
 //  Created by Luis Genaro Arvizu Vega on 06/03/18.
-//  Copyright © 2018 HP501865. All rights reserved.
+//  Copyright © 2018 Genaro. All rights reserved.
 //
 
 import UIKit
@@ -11,10 +11,19 @@ import CoreImage
 
 extension UIImage{
     
-    
-    func configuration(){
+    func GARFilter() -> UIImage?{
+        guard let filter = CIFilter(name: "CIPhotoEffectNoir"), let ciImage = CIImage.init(image: self) else{
+            return nil
+        }
         let context = CIContext()
-        //let filter = CIFilter.filterNames(inCategory: x)
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        if let imageOutPut = filter.outputImage?.applyingFilter("CIColorControls", parameters: [kCIInputContrastKey: 3, kCIInputBrightnessKey: 0.98, kCIInputSaturationKey: 0]), let image = context.createCGImage(imageOutPut, from: imageOutPut.extent){
+            
+            let result = UIImage.init(cgImage: image, scale: 1.0, orientation: .up)
+            return result
+            
+        }
+        return nil
     }
     
 }
