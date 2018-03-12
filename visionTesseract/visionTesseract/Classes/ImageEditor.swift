c841001b0e400653ffa3d95dddbfa72735816ddc
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-03-12 11:27:06 -0600

    Se agregan propiedades a los UIImage
    Las propiedades agregadas fueron:
    Convertir a JPEG
    Convertir a PNG
    obtener el brillo
    
    GAR.

--- visionTesseract/visionTesseract/Classes/ImageEditor.swift
+++ visionTesseract/visionTesseract/Classes/ImageEditor.swift
@@ -11,6 +11,34 @@ import CoreImage
 
 extension UIImage{
     
+    var jpeg: Data?{
+        return UIImageJPEGRepresentation(self, 1.0)
+    }
+    
+    var png: Data?{
+        return UIImagePNGRepresentation(self)
+    }
+    
+    var brightness: Double?{
+        let context = CIContext()
+        guard let image = CIImage(image: self) else {return nil}
+        guard let cgImage = context.createCGImage(image, from: image.extent) else {return nil}
+        let width = cgImage.width
+        let height = cgImage.height
+        let pixels = CFDataGetLength(cgImage.dataProvider!.data!)
+        let pixel =  CFDataGetBytePtr(cgImage.dataProvider!.data!)
+        var brightness: Double = 0
+        for count in stride(from: 0, to: pixels, by: 4){
+            let r = pixel![count]
+            let g = pixel![count+1]
+            let b = pixel![count+2]
+            brightness += (Double(r)*0.299 + Double(g)*0.587 + Double(b)*0.114)
+        }
+        brightness /= Double(width * height)
+        brightness /= 255
+        return brightness
+    }
+    
     func GARFilter() -> UIImage?{
         guard let filter = CIFilter(name: "CIPhotoEffectNoir"), let ciImage = CIImage.init(image: self) else{
             return nil
